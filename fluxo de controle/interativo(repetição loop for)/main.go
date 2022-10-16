ee0aa712985213ce465cea670ee59f1f515172f0
gabrielluizsf <gabrielluizads123@gmail.com>
2022-10-16 18:32:17 -0300

    interativo &  sequencial

---
+++ fluxo de controle/interativo(repetição loop for)/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Golang não tem while");
+}
+func clock(){
+	for hour := 0; hour <= 12; hour ++{	
+		fmt.Println("Hora:",hour,":00");
+		for minutes := 0; minutes < 60; minutes ++{
+			fmt.Print(hour,":",minutes,"\n");
+		}
+	}
+}
+func basicLoop(){
+	for x := 0; x < 10; x++{
+		fmt.Println(x);
+	}
+}
+func fakeWhile(){
+	x := 0;
+	for x<10{
+		fmt.Println("[X menor que 10]","\tValor de X:", x)
+		x++
+	}
+}
+
+func infiniteLoop(){
+	for {
+		fmt.Println("Ctrl+C to break infinite for")
+	}
+}
+func endingInfiniteLoop(){
+		x := 0
+	for{
+		if x < 10{
+			fmt.Println("[X menor que 10]","\tValor de X:", x);
+			x++
+		}else{
+			fmt.Println("[X é igual a 10]","\tValor de X:",x,"\nBye");
+			break
+		}
+	}
+}
