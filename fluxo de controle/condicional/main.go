037bfaf16a018b95ee1678a3644e62c51d7dca9a
gabrielluizsf <gabrielluizads123@gmail.com>
2022-10-16 18:33:35 -0300

    condicional

---
+++ fluxo de controle/condicional/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	isTrue();
+}
+func isTrue(){
+	booleano := true
+	if booleano == true{
+		fmt.Println("yes")
+	}else{
+		fmt.Println("no")
+	}
+
+
+}
