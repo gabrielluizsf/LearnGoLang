904431150ad50ddce5af4ded5d3bbc41415ca0da
Gabriel Luiz <939672097+GabrielLuizSF@users.noreply.github.com>
2022-10-19 13:22:29 -0300

    Condicional implementado

--- fluxo de controle/condicional/main.go
+++ fluxo de controle/condicional/main.go
@@ -6,6 +6,8 @@ import (
 
 func main(){
 	isTrue();
+    Loading(10);
+    Cases("apple")
 }
 func isTrue(){
 	booleano := true
@@ -14,6 +16,32 @@ func isTrue(){
 	}else{
 		fmt.Println("no")
 	}
+}
+//! == NOT
+func Loading(value int){
+  if !(value > 100){
+      fmt.Println("Loading....")
+  }else if value > 100{
+      fmt.Println("Invalid Value")
+  }else{
+        fmt.Printf("100%")
+    }
+}
 
+func Cases(choice string){
+
+    switch choice{
+    case "bread":
+        fmt.Println("Bread: R$2.00");
+    case "apple":
+        fmt.Println("Apple: R$10.00")
+        fallthrough
+    case "limon":
+        fmt.Println("Limon: FREE");
+    case "foods":
+        fmt.Println("Foods: bread | apple")
+    default:
+        fmt.Println("Invalid Choice")
+    }
 
 }
