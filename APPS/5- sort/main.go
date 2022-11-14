6874a01b0e747269738486e4e13c94502b298dde
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-14 17:08:44 -0300

    utilizando o sort customizado

--- APPS/5- sort/main.go
+++ APPS/5- sort/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"sort"
+	custom "package-sort/custom"
 )
+
 
 func main(){
 
+carros := []custom.Carro{
+		custom.Carro{"S10",197,5},
+		custom.Carro{"Porsche",530,2},
+		custom.Carro{"fusca",20,30},
+	};	
+	custom.MySort(carros)
+	//sortExamples();
+}
+
+func sortExamples(){ 
 frase   := []string{"Dia","Bom"};
 numbers := []int{10,80,90,27,2,5,1,4,3,6,7,8,9,30};
 myNumber := 4;
@@ -31,4 +43,5 @@ func SearchMyNumber(numbers []int,myNumber int){
 	} else {
 		fmt.Printf("%d not found in %v\n",myNumber,numbers);
 	}
+	
 }
