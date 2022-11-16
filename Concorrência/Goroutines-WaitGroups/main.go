9f4e3f79d2adfc461eeade8033ba0ae9e0e64249
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-16 10:28:32 -0300

    Goroutines && WaitGroups

---
+++ Concorrência/Goroutines-WaitGroups/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"runtime"
+)
+
+/*
+Goroutines: Linhas encadeamentos de funções
+ex: varios sistemas separados rodando independentemente;
+WaitGroups:
+wg  sync.WaitGroup == Grupo de Espera;
+func Add(){} : Define quantas goroutines
+func Done(){}: Finilizar o processo
+func Wait(){}: Aguarda a finalização das goroutines
+ex: esperar os sistemas serem executados
+*/
+var(
+	waitGroup sync.WaitGroup
+)
+
+func main(){
+	//printNumCPUs();
+	//printGoroutines();
+	waitGroup.Add(2);
+	go rocket();
+	go whoami();
+	//printGoroutines();
+	waitGroup.Wait()
+}
+
+func rocket(){
+	fmt.Printf("🚀");
+	waitGroup.Done();
+}
+func printGoroutines(){	fmt.Println(runtime.NumCPU());}
+func printNumCPUs(){fmt.Println(runtime.NumGoroutine());}
+func whoami(){
+	name := "";
+	fmt.Printf("Digite seu nome:");
+	fmt.Scanf("%s",&name);
+	fmt.Println("Hello",name);
+	waitGroup.Done();
+} 
