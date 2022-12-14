8f75b38b5644c96dee471caf2314f656338ca032
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-12-14 03:45:58 -0300

    Lvl9 ex3

---
+++ exercicios/[LVL9]/ex3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	waitGroup sync.WaitGroup
+	counter   int64
+)
+func main(){
+	generateGoroutines(50);
+	waitGroup.Wait();
+	fmt.Println("Contador:",counter);
+}
+
+func generateGoroutines(number int){
+	waitGroup.Add(number);
+	for count := 0; count < number; count ++{
+	go func(){
+		value := counter;
+		runtime.Gosched();
+		value++
+		counter = value;
+		waitGroup.Done();
+	}()
+	}
+}
