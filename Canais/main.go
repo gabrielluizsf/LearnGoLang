864001ef33575f2f06115256d3f9671daf739e83
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-12-22 11:48:23 -0300

    Update Main Function

--- Canais/main.go
+++ Canais/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/GabrielLuizSF/LearnGoLang/Canais/canais_direcionais"
   "github.com/GabrielLuizSF/LearnGoLang/Canais/channel"
+  "github.com/GabrielLuizSF/LearnGoLang/Canais/channel/context"
 )
 
 func main(){
@@ -22,7 +23,17 @@ func main(){
 	channelsBuffer(5, 1);
 
 	canais_direcionais.BiDirectionalChannel();
+
+  channel.StartChannel(5000);
   channel.RangeChannel();
+  channel.SelectChannel(5000);
+  channel.NumbersChannel(50);
+  channel.CommaOK(55);
+  channel.ConvergeChannel(500);
+  channel.DivergeChannel(100);
+  channel.GoFuncCreator(50);
+  context.MainContext();
+  channel.WorkChannel(100);
 }
 
 func channelsBuffer(number int, bufferNumber int){
