6bbc35350cafd61249ce5bd30be44469650005d3
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-12-22 11:45:04 -0300

    Context WithTimeout

---
+++ Canais/channel/context/Timeout.go
@@ -0,0 +1,21 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func timeoutContext() {
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err()) // prints "context deadline exceeded"
+	}
+
+}
+
