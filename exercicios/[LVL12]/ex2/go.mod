d99f200178e266659cfecc879b767758daba27e0
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-23 12:01:22 -0300

    Updating package

--- exercicios/[LVL12]/ex2/go.mod
+++ exercicios/[LVL12]/ex2/go.mod
@@ -1,3 +1,5 @@
-module github.com/gabrielluizsf/errorrr
+module github.com/gabrielluizsf/LearnGoLang/exercicios/LVL12/ex2
 
 go 1.19
+
+require github.com/theGOURL/errorrr v0.0.0-20230123145033-8b7eda2c2c07 // indirect
--- exercicios/[LVL12]/ex2/main.go
+++ exercicios/[LVL12]/ex2/main.go
@@ -1,8 +1,23 @@
+package main
+/*
 package errorrr
+Esta função retorna no console a mensagem customizada e o erro se ocorrer erro
 
-import "fmt"
-
-//Esta função retorna no console a mensagem customizada e o erro se ocorrer erro
-func printCustomMessageAndErrors(message string, err error){
+func PrintCustomMessageAndErrors(message string, err error){
   fmt.Println(message,err);
+}
+*/
+
+import (
+	"os/user"
+	"github.com/theGOURL/errorrr"
+)
+
+// this function prints the username of the system,
+// returning the username
+func main() string {
+	whoami, err := user.Current();
+  errorrr.PrintCustomMessageAndErrors("ERROR: ",err);
+	username := whoami.Username;
+	return username;
 }
