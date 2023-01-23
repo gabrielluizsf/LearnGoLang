602d62540be155eee874fe014b2e07b2f6309c9d
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-23 11:35:12 -0300

    Adding ex1 finished

--- exercicios/[LVL12]/ex1/cachorro/main.go
+++ exercicios/[LVL12]/ex1/cachorro/main.go
@@ -2,6 +2,6 @@ package cachorro
 //cmd/godoc: deprecate and point to cmd/pkgsite
 //Esta Função retorna a idade do cachorro em anos caninos
 func Idade(years_number int) int{
-  //os anos recebidos por paramêtro são retornados +7 anos para gerar os anos caninos
-  return years_number + 7
+  //os anos recebidos por paramêtro são retornados *7 anos para gerar os anos caninos
+  return years_number * 7;
 }
