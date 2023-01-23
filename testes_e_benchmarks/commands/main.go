ff4d620ce6e4e6b0db159e72c9cb84cc5361a2a6
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-23 17:42:26 -0300

    Alguns comandos uteis

---
+++ testes_e_benchmarks/commands/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+  fmt.Println(`
+     --->>>>     COMMANDS
+     --------------------------------------
+    |    gofmt  -> formata seu código      |
+    |go vet -> procura constructs suspeitos|
+    |golint -> procura code style ruim     |
+     --------------------------------------
+    `)
+}
